83fd77f2001c48a2f8ada0e81f4d97ffbe8950dc
gigix74 <gigi@laptop-hm.localdomain>
2019-05-18 09:37:30 +0300

    L298 Test

---
+++ 190518/L298Test/L298Test.ino
@@ -0,0 +1,36 @@
+byte IN1 = 4;
+byte IN2 = 5;
+byte IN3 = 7;
+byte IN4 = 6;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN4, LOW);
+  delay(1000);
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN4, LOW);
+  delay(1000);
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN2, HIGH);
+  digitalWrite(IN4, HIGH);
+  delay(1000);
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN4, LOW);
+  delay(1000);
+}
